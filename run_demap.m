d6c33f1b8471a63546183e6832571f6e2cd32c2e
solidus66 <66solidus@gmail.com>
2024-04-20 18:37:51 +0300

    added some tests

--- run_demap.m
+++ run_demap.m
@@ -1,50 +1,58 @@
-% %% Запуск demap8PSK_Rmatrix.m
-% y = [1+1i, -1-1i, 1-1i, -1+1i]; % примерный вектор комплексных чисел
-% sigma2 = 0.1; % примерное значение дисперсии шума
-% 
-% llr = demap8PSK_Rmatrix(y, sigma2);
-% 
-% disp('Мягкие решения (LLR):');
-% disp(llr);
+%% Тест demap8PSK_Rmatrix.m
+angles = (-7:2:7) * pi/8; % от -7pi/8 до 7pi/8 с шагом pi/4
 
+noise_levels = [0.1, 0.05, 0.2]; % очень низкий (близкий к нулю), низкий шум, высокий шум
 
-% Параметры сигнала
-M = 8; % 8PSK, так что M = 8
-phi = linspace(0, 2*pi, M+1); % Генерация M равномерно распределенных фаз от 0 до 2*pi
-phi(end) = []; % Удалить последнюю точку, так как она совпадает с первой
+% перебор уровней шума
+for sigma2 = noise_levels
+    fprintf('Тест при уровне шума sigma2 = %f\n', sigma2);
+    for i = 1:length(angles)
+        % идеальные координаты точки
+        yI = cos(angles(i));
+        yQ = sin(angles(i));
+        
+        % добавление шума
+        yI_noisy = yI + sigma2 * randn();
+        yQ_noisy = yQ + sigma2 * randn();
+        
+        % вычисление LLR
+        LLRs = demap8PSK_Rmatrix(yI_noisy, yQ_noisy, sigma2);
+        
+        fprintf('Угол: %5.2f, LLR: [%6.2f, %6.2f, %6.2f]\n', angles(i), LLRs);
+    end
+    fprintf('\n');
+end
+fprintf('__________\n')
+%% Тест 2
+% точки созвездия 8PSK для тестирования
+angles = (-7:2:7) * pi/8; % От -7pi/8 до 7pi/8 с шагом pi/4
+sigma2 = 0.1;
 
-% Создание комплексных точек сигнала
-s = exp(1i * phi); % Идеальные точки созвездия
+% проходим по всем тестовым точкам
+for i = 1:length(angles)
+    yI = cos(angles(i));
+    yQ = sin(angles(i));
+    
+    % вызываем функцию demap8PSK_Rmatrix
+    LLRs = demap8PSK_Rmatrix(yI, yQ, sigma2);
 
-% Добавление шума
-sigma = 1; % Уровень шума
-noise = sigma * (randn(size(s)) + 1i * randn(size(s))); % Гауссовский шум
-y = s + noise; % Сигнал с шумом
-
-% Сохранение тестовых данных
-save('test_data.mat', 'y', 'sigma');
-% Загрузка тестовых данных
-load('test_data.mat');
-
-% Вызов функции демодуляции
-LLRs = demap8PSK_Rmatrix(y, sigma);
-
-% Вывод результатов
-disp('Рассчитанные LLR:');
-disp(LLRs);
-
-
+    fprintf('Тестовый угол: %5.2f, LLR: [%6.2f, %6.2f, %6.2f]\n', angles(i), LLRs);
+end
+fprintf('__________\n')
 %% Запуск getRMatrix.m
-% % region = input('Укажите область (1-8): ');
-% % R_matrix_area = getRMatrix(region);
-% % R_matrix_area = getRMatrix(1);
-% % disp('R-матрица для указанной области');
-% disp('R-матрица')
-% disp(getRMatrix(1));
-% disp(getRMatrix(2));
-% disp(getRMatrix(3));
-% disp(getRMatrix(4));
-% disp(getRMatrix(5));
-% disp(getRMatrix(6));
-% disp(getRMatrix(7));
-% disp(getRMatrix(8));
+num_regions = 8; % общее количество областей
+for region = 1:num_regions
+    disp(['R-матрица для области ', num2str(region), ':']);
+    disp(getRMatrix(region));
+end
+fprintf('__________\n')
+%% Проверка determineRegion.m
+test_angles = [-pi, -3*pi/4, -pi/2, -pi/4, 0, pi/4, pi/2, 3*pi/4, pi, -7*pi/8];
+expected_regions = [1, 2, 3, 4, 5, 6, 7, 8, 1, 1];
+
+for i = 1:length(test_angles)
+    calculated_region = determineRegion(test_angles(i));
+    fprintf('Угол: %5.2f, Ожидаемая область: %d, Вычисленная область: %d\n', ...
+            test_angles(i), expected_regions(i), calculated_region);
+end
+fprintf('__________\n')
