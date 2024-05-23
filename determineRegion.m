8a1329cc8c28bc4edfa66a09b7fac4a51c2ff84f
solidus66 <66solidus@gmail.com>
2024-05-23 22:40:07 +0300

    edited formatting

--- determineRegion.m
+++ determineRegion.m
@@ -1,13 +1,13 @@
 function region = determineRegion(phaseAngle)
-    % нормализация угла фазы в диапазон [-pi, pi)
-    phaseAngle = mod(phaseAngle + pi, 2*pi) - pi;
+% Нормализация угла фазы в диапазон [-pi, pi)
+phaseAngle = mod(phaseAngle + pi, 2*pi) - pi;
 
-    % определение границ для всех восьми областей
-    region_bounds = [-pi, -3*pi/4, -pi/2, -pi/4, 0, pi/4, pi/2, 3*pi/4];
+% Определение границ для всех восьми областей
+region_bounds = [-pi, -3*pi/4, -pi/2, -pi/4, 0, pi/4, pi/2, 3*pi/4];
 
-    % определение области фазового угла
-    region = find(phaseAngle >= region_bounds, 1, 'last');
-    if isempty(region)
-        region = 1;
-    end
+% Определение области фазового угла
+region = find(phaseAngle >= region_bounds, 1, 'last');
+if isempty(region)
+    region = 1;
+end
 end
